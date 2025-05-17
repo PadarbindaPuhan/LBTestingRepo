5e32470f65ed5cd533625c4ec6750e68067b9097
Dilip <dilip.samanta@gmail.com>
2025-05-17 18:48:56 +0530

    create server

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -8,21 +8,7 @@ variable "keypair_public_key" {
   type        = string
 }
 
-# variable "instance_count" {
-#   description = "Number of instances to create"
-#   type        = number
-#   default     = 1
-# }
-
-
-# Add this block if it doesn't already exist
-# variable "tag_name" {
-#   description = "Tags to apply to the EC2 instances"
-#   type        = map(string)
-#   default     = {}
-# }
-
 variable "instance_names" {
   type    = list(string)
-  default = ["web-1", "web-2", "web-3", "web-4", "web-5"]
+  default = ["web-1", "web-2"]
 }
