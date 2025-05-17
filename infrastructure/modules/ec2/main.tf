b15ecbd8d39a074b894b1d8d65b51100788a07eb
Dilip <dilip.samanta@gmail.com>
2025-05-17 18:35:50 +0530

    fix code

--- infrastructure/modules/ec2/main.tf
+++ infrastructure/modules/ec2/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web_app" {
   vpc_security_group_ids = var.security_groups
   key_name      = var.key_pair
   tags = {
-    Name        = "${var.environment}-${tag_name}-web-app-instance"
+    Name        = "${var.environment}-${var.tag_name}-web-app-instance"
     Environment = var.environment
   }
 } 
