b63693a055e52ebdd33f57b299abfadd1e1bc990
Dilip <dilip.samanta@gmail.com>
2025-05-18 21:17:49 +0530

    fixing code

--- infrastructure/modules/ec2/output.tf
+++ infrastructure/modules/ec2/output.tf
@@ -1,9 +1,3 @@
-output "instance_ip_address" {
-  description = "The public IP address of the web app instance"
-  value       = aws_instance.web_app.public_ip
-  
-}
-
 output "instance_private_ip" {
   description = "The ID of the web app instance"
   value       = aws_instance.web_app.private_ip
