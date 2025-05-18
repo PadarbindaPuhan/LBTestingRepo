4559fb9cdb0decca2e46beaf0c2d598bc44abc56
Dilip <dilip.samanta@gmail.com>
2025-05-18 22:11:27 +0530

    fixing code

--- infrastructure/application/green/main.tf
+++ infrastructure/application/green/main.tf
@@ -6,3 +6,18 @@ module "greens" {
   key_pair = var.key_pair
   tag_name = var.tag_name
 } 
+
+# # results of the above code in a array
+locals {
+  all_green_servers = [for m in values(module.blues) : m.instance_id]
+}
+
+
+module "blue_target_group" {
+  source = "../../modules/target_group"
+  name = "blue-target-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = var.vpc_id
+  target_ids =  local.all_green_servers
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,3 +23,14 @@ module "blue_servers" {
   
 }
 
+module "green_servers" {
+  blue_servers = var.green_servers
+  source        = "./application/blue"
+  ami           = "ami-075686beab831bb7f"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-0db2c89226baaf431"
+  environment   = var.environment
+  security_groups = [module.allow_all_security_group.security_group_id]
+  key_pair      = module.keypair.key_name
+  
+}
