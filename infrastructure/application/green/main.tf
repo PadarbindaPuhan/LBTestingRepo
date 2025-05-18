8ee8e04437757616b181328d2fd2dbd39dbdc77f
PadarbindaPuhan <Padarbinda.Puhan@gmail.com>
2025-05-19 00:01:24 +0530

    fixed instance name

--- infrastructure/application/green/main.tf
+++ infrastructure/application/green/main.tf
@@ -1,10 +1,11 @@
 module "greens" {
   for_each      = toset(var.green_servers)
+  tag_name = each.value
   source = "../../modules/ec2"
   environment = var.environment
   security_groups = var.security_groups
   key_pair = var.key_pair
-  tag_name = var.tag_name
+  #tag_name = var.tag_name
 } 
 
 # # results of the above code in a array
