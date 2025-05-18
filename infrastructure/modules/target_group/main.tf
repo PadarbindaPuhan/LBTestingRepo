616f4447bb39cf63f26c5755a3452bb787677d04
Dilip <dilip.samanta@gmail.com>
2025-05-18 22:00:03 +0530

    fixing code

--- infrastructure/modules/target_group/main.tf
+++ infrastructure/modules/target_group/main.tf
@@ -6,7 +6,7 @@ resource "aws_lb_target_group" "tg" {
 }
 
 resource "aws_lb_target_group_attachment" "tg_attachment" {
-  for_each        = toset(var.target_ids)
+  for_each        = tomap(var.target_ids)
   target_group_arn = aws_lb_target_group.tg.arn
   target_id       = each.value
   port            = var.port
